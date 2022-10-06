4137f3bb823238a8b6097b9c78677043bd6bd041
wheemyungshin <zerger26@gmail.com>
2022-10-06 17:34:36 +0900

    min box size

--- mmpose/datasets/datasets/top_down/topdown_coco_dataset.py
+++ mmpose/datasets/datasets/top_down/topdown_coco_dataset.py
@@ -176,7 +176,6 @@ class TopDownCocoDataset(Kpt2dSviewRgbImgTopDownDataset):
             })
             bbox_id = bbox_id + 1
 
-        print(rec[0]['bbox'])
         return rec
 
     def _load_coco_person_detection_results(self):
